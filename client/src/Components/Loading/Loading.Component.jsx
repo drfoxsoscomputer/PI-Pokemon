7ad163976a27d80da57ea783061ed06e1909b1e8
DrFox <daprthefox@gmail.com>
2023-10-30 18:56:45 -0400

    Limpiando codigo que ya no se usa

--- client/src/Components/Loading/Loading.Component.jsx
+++ client/src/Components/Loading/Loading.Component.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './Loading.Styles.css';
-import loading from "../../assets/img/loading.gif"
+import React from "react";
+import "./Loading.Styles.css";
+import loading from "../../assets/img/loading.gif";
 
 const Loading = () => {
   return (
     <div className="loader-container">
-      {/* <div className="loader"></div> */}
-      <img className="loader" src={loading} alt="loading" />
+      <img
+        className="loader"
+        src={loading}
+        alt="loading"
+      />
       <h3>Loading...</h3>
     </div>
   );
